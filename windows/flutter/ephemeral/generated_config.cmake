02202a47d061787741c79f4516f3eccac93ae46d
Utsmay Kumar <141228940+utsk17@users.noreply.github.com>
2023-10-10 03:13:01 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\gandh\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\DiceGame\\dice_game" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\gandh\\src\\flutter"
+  "PROJECT_DIR=D:\\DiceGame\\dice_game"
+  "FLUTTER_ROOT=C:\\Users\\gandh\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\DiceGame\\dice_game\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\DiceGame\\dice_game"
+  "FLUTTER_TARGET=D:\\DiceGame\\dice_game\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\DiceGame\\dice_game\\.dart_tool\\package_config.json"
+)
